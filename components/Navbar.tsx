537669756d274833d7763f299faafa00e31feaae
Al Amin <alaminpgmr@gmail.com>
2023-10-15 11:51:12 +0600

    navbar done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
+const navIcons = [
+  {
+    src: "/assets/icons/search.svg",
+    alt: "search",
+    href: "/search",
+  },
+  {
+    src: "/assets/icons/black-heart.svg",
+    alt: "heart",
+    href: "/wishlist",
+  },
+  {
+    src: "/assets/icons/user.svg",
+    alt: "user",
+    href: "/account",
+  },
+];
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image src="/assets/icons/logo.svg" width={27} height={27} alt="logo" />
+          <p className="nav-logo">
+            Price<span className="text-primary">Wise</span>
+          </p>
+        </Link>
+        <div className="flex items-center gap-5">
+          {navIcons.map(({ src, alt, href }) => (
+            <Link key={alt} href={href} className="nav-icon">
+              <Image src={src} width={28} height={28} alt={alt} />
+            </Link>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
